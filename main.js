552088114caee7cb00d27b4969fafe244647d8cd
matzzp <matzz_p@live.com>
2026-06-23 17:52:36 -0300

    correccion

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 
-let AnioActual = 2026;
+const AnioActual = 2026;
 let Nombre = prompt("Ingrese su nombre");
 let Apellido = prompt("Ingrese su apellido");
 let AnioNacimiento = prompt("Ingrese su anio de nacimiento");
 
 let Edad = AnioActual - parseInt(AnioNacimiento);
-
+    console.log(Edad)
 if (isNaN(Edad)){
     alert("No ingresaste la edad con numeros");
 }else{
 
-    let mensaje = "estos datos son correctos? \n";
-    mensaje = mensaje + "Nombre: " + Nombre + "\n";
-    mensaje = mensaje + "Apellido: " + Apellido + "\n";
-    mensaje = mensaje + "Edad: " + Edad + "\n";
+    let Mensaje = "estos datos son correctos? \n";
+    Mensaje = Mensaje + "Nombre: " + Nombre + "\n";
+    Mensaje = Mensaje + "Apellido: " + Apellido + "\n";
+    Mensaje = Mensaje + "Edad: " + Edad + "\n";
     
-    let respuesta = confirm (mensaje);
+    let Respuesta = confirm (Mensaje);
 
-    if (respuesta) {
+    if (Respuesta) {
         alert ("bienvenido");
 
     }else{
